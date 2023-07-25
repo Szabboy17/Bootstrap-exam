90e3ebd0a994f78e3be908bca2f226bf85c3cda2
Szabboy17 <129592254+Szabboy17@users.noreply.github.com>
2023-07-25 23:49:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function Click(){
+    if(Click){
+        window.location = "lorem.html"
+    }
+}
+
+function Click2(){
+    if(Click){
+        window.location = "index.html"
+    }
+}
+
+function Click3(){
+    if(Click){
+        window.location = "index404.html"
+    }
+}
